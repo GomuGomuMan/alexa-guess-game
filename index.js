e66247395e826c3cc1b4fb600b0daee1e4cf29b5
GomuGomuMan <lucky7duy211@gmail.com>
2017-07-01 22:10:22 -0700

    Changed config to env var

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const intentHandlers = require('./intents');
 
 exports.handler = (event, context, callback) => {
   var alexa = Alexa.handler(event, context);
-  alexa.appId = constants.get('appId');
-  alexa.dynamoDBTableName = constants.get('dynamoDBTableName');
+  // alexa.appId = constants.get('appId');
+  alexa.appId = process.env.appId;
+  alexa.dynamoDBTableName = process.env.dynamoDBTableName;
 
   alexa.registerHandlers(intentHandlers); //intentHandlers
   alexa.execute();
